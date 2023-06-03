dc161c275a8583b09565e1eb4d50121d41675d85
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-06-03 16:54:18 -0700

    added beginning code to project

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,51 @@
+import java.util.Random;
+
+const LETTERPOOL = {
+  'A': 9, 
+  'B': 2, 
+  'C': 2, 
+  'D': 4, 
+  'E': 12, 
+  'F': 2, 
+  'G': 3, 
+  'H': 2, 
+  'I': 9, 
+  'J': 1, 
+  'K': 1, 
+  'L': 4, 
+  'M': 2, 
+  'N': 6, 
+  'O': 8, 
+  'P': 2, 
+  'Q': 1, 
+  'R': 6, 
+  'S': 4, 
+  'T': 6, 
+  'U': 4, 
+  'V': 2, 
+  'W': 2, 
+  'X': 1, 
+  'Y': 2, 
+  'Z': 1
+};
 export const drawLetters = () => {
-  // Implement this method for wave 1
+    let lettersList = [];
+    for (const[key,value] of Object.entries(LETTERPOOL)){
+      lettersList.push([key] * value);
+    }
+    let hand = [];
+
+    // for (let step = 0; step < 11; step ++){
+    //   let randomLetter = random.choice(lettersList);
+    //   hand.push(randomLetter)
+    //   lettersList.splice(randomLetter)
+    // function choose(choices) {
+    //   const index = Math.floor(Math.random() * choices.length);
+    //   return choices[index];
+    // }
+    }
+    return hand
+
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
